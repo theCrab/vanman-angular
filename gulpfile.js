bf033ecc4181b607b75d03e48436fd83cf736ff1
The Crab <nelson@safarista.com>
2016-09-06 17:24:14 +0100

    Add angularjs, initial app setup and gulp tasks to compile browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,26 @@ var gulp    = require('gulp')
 var sass    = require('gulp-ruby-sass')
 var connect = require('gulp-connect')
 
+// requires browserify and vinyl-source-stream
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+
+// Connect task
 gulp.task('connect', function () {
 	connect.server({
 		root: 'public',
 		port: 5000
 	})
+})
+
+gulp.task('browserify', function() {
+	// Grabs the app.js file
+	return browserify('./app/app.js')
+
+  	// bundles it and creates a file called main.js
+  	.bundle()
+  	.pipe(source('main.js'))
+
+  	// saves it to public/javascripts/ directory
+  	.pipe(gulp.dest('./public/javascripts/'))
 })
